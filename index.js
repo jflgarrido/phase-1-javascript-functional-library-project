974eab0a81acddb65f08c03be49de8c7798e39f8
Julius Garrido <jflgarrido@gmail.com>
2021-12-15 20:52:25 -0700

    commit tree 4a493625343aa861a0c87fbd33949c43fd661880

--- index.js
+++ index.js
@@ -22,19 +22,69 @@ function myMap(collection, callbackFn){
 
 function myReduce(collection, callback, acc){
     let isArray = collectionIsArray(collection)
-    let accumulator = 0
-    for(const item of isArray){
-            let newAcc = callback(acc, item, isArray)
-            console.log(newAcc)
-            let accumulator =+ newAcc
-            console.log(accumulator)
+    if(!acc){
+        acc = isArray[0]
+        isArray = isArray.slice(1)
     }
-    return accumulator
+    for(const item of isArray){
+        acc = callback(acc, item, isArray)
+    }
+    return acc
 }
 
 function myFind(collection, predicate){
     let IsArray = collectionIsArray(collection)
     for(const item of IsArray){
-        predicate(item)
+        if (predicate(item)){
+            return item
+        }
     }
+    return undefined
 } 
+
+function myFilter(collection, predicate){
+    let IsArray = collectionIsArray(collection)
+    let newArray = []
+    for(const item of IsArray){
+        if (predicate(item)){
+            newArray.push(item)
+        }
+    }
+    return newArray
+}
+
+function mySize(collection){
+    let IsArray = collectionIsArray(collection)
+    return IsArray.length
+}
+
+function myFirst(array, n){
+    if(!n){
+         return array[0]
+    }
+    return array.slice(0, n)
+}
+
+function myLast(array, n){
+    if(!n){
+        let arrayLength = array.length
+        return array[(arrayLength)-1]
+   }
+   return array.slice(-n)
+}
+
+function myKeys(obj){
+    let ObjArray = []
+    for(const [key] of Object.entries(obj)){
+        ObjArray.push(key)
+    }
+    return ObjArray
+}
+
+function myValues(obj){
+    let ObjArray = []
+    for(const [key, value] of Object.entries(obj)){
+        ObjArray.push(value)
+    }
+    return ObjArray
+}
